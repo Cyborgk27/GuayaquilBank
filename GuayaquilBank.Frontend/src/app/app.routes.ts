8c4218380c76978ec888c97426d8d6542a65b8e6
kcepeda <slenderstalin@hotmail.com>
2026-06-05 17:40:40 -0500

    feat(auth): se agrega la pantalla de login

--- GuayaquilBank.Frontend/src/app/app.routes.ts
+++ GuayaquilBank.Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    title: 'Iniciar Sesión - Guayaquil Bank ERP',
+    loadComponent: () => import('./features/auth/pages/login/login')
+      .then(m => m.Login)
+  },
+];
---
+++ GuayaquilBank.Frontend/src/app/features/auth/pages/login/login.ts
@@ -0,0 +1,52 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthFacade } from '../../../../core/service/auth.facade';
+import { LoginRequestDto } from '../../../../core/api/v1';
+import { GenericInput } from '../../../../shared/components/generic-input/generic-input';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, GenericInput],
+  templateUrl: './login.html',
+})
+export class Login {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthFacade);
+  private router = inject(Router);
+
+  public isLoading = signal<boolean>(false);
+
+  public loginForm: FormGroup = this.fb.nonNullable.group({
+    domain: ['', [Validators.required, Validators.minLength(3)]],
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  public onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    this.isLoading.set(true);
+    const credentials = this.loginForm.getRawValue() as LoginRequestDto;
+
+    this.authService.login(credentials).subscribe({
+      next: () => {
+        this.router.navigate(['/dashboard']);
+      },
+      error: () => {
+        this.isLoading.set(false);
+      },
+      complete: () => {
+        this.isLoading.set(false);
+      }
+    });
+  }
+
+  public isFieldInvalid(field: string): boolean {
+    const control = this.loginForm.get(field);
+    return !!(control && control.errors && (control.dirty || control.touched));
+  }
+}
