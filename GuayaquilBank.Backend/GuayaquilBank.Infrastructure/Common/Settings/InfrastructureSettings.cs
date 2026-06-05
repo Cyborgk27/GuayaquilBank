20f47e9bb0c0cc2e1fb2f1371480c77809d1adbc
kcepeda <slenderstalin@hotmail.com>
2026-06-04 19:17:17 -0500

    feat(common): se agrega clases para el pattern configure

---
+++ GuayaquilBank.Backend/GuayaquilBank.Infrastructure/Common/Settings/InfrastructureSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuayaquilBank.Infrastructure.Common.Settings
+{
+    public class InfrastructureSettings
+    {
+        public string DatabaseProvider { get; set; } = null!;
+        public SecuritySettings Security { get; set; } = new();
+        public CorsSettings Cors { get; set; } = new();
+    }
+}
---
+++ GuayaquilBank.Backend/GuayaquilBank.Infrastructure/Common/Settings/SecuritySettings.cs
@@ -0,0 +1,8 @@
+﻿namespace GuayaquilBank.Infrastructure.Common.Settings
+{
+    public class SecuritySettings
+    {
+        public int HashWorkFactor { get; set; } = 12;
+        public JwtSettings Jwt { get; set; } = new();
+    }
+}
