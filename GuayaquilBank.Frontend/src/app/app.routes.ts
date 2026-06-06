f65855ec11ced67117bcf433fd382d9f2a32e550
kcepeda <slenderstalin@hotmail.com>
2026-06-06 00:05:27 -0500

    feat(sales): se agrega la lista de faturas

--- GuayaquilBank.Frontend/src/app/app.routes.ts
+++ GuayaquilBank.Frontend/src/app/app.routes.ts
@@ -11,13 +11,19 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./shared/components/layout/layout/layout')
       .then(m => m.Layout),
-      children: [
-        {
+    children: [
+      {
         path: 'customers',
         title: 'Gestión de Clientes - Guayaquil Bank ERP',
         loadComponent: () => import('./features/customers/pages/customer-list/customer-list')
           .then(m => m.CustomerList)
       },
-      ]
+      {
+        path: 'sales',
+        title: 'Gestión de Facturas - Guayaquil Bank ERP',
+        loadComponent: () => import('./features/invoices/pages/invoice-list/invoice-list')
+          .then(m => m.InvoiceList)
+      }
+    ]
   }
 ];
---
+++ GuayaquilBank.Frontend/src/app/features/invoices/pages/invoice-list/invoice-list.ts
@@ -0,0 +1,113 @@
+import { Component, inject, signal, OnInit, effect, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SalesApiService, InvoiceResponseDto } from '../../../../core/api/v1'; // Ajusta la ruta según tu SDK
+import { GenericTable } from '../../../../shared/components/generic-table/generic-table';
+import { TableColumn } from '../../../../shared/interfaces/table-column.interface';
+import { TableAction } from '../../../../shared/interfaces/table-action.interface';
+import { DataFilter } from '../../../../shared/components/data-filter/data-filter';
+import { GenericModal } from "../../../../shared/components/generic-modal/generic-modal";
+import { InvoicePdfViewer } from '../../components/invoice-pdf-viewer/invoice-pdf-viewer';
+
+@Component({
+  selector: 'app-invoice-list',
+  standalone: true,
+  imports: [CommonModule, GenericTable, DataFilter, GenericModal, InvoicePdfViewer],
+  templateUrl: './invoice-list.html'
+})
+export class InvoiceList implements OnInit {
+  @ViewChild('pdfModal') pdfModal!: GenericModal;
+
+  private salesService = inject(SalesApiService);
+
+  public invoices = signal<InvoiceResponseDto[]>([]);
+  public isLoading = signal<boolean>(false);
+
+  // Parámetros de paginación y búsqueda reactiva
+  public page = signal<number>(1);
+  public pageSize = signal<number>(5);
+  public search = signal<string>('');
+
+  public totalItems = signal<number>(0);
+  public totalPages = signal<number>(0);
+
+  public selectedInvoiceId = signal<string | null>(null);
+
+  public tableColumns: TableColumn[] = [
+    { key: 'invoiceNumber', label: 'Nº Comprobante' },
+    { key: 'clientName', label: 'Cliente / Razón Social' },
+    {
+      key: 'issuedAt',
+      label: 'Fecha Emisión',
+    },
+    {
+      key: 'total',
+      label: 'Total ($)',
+      badgeClass: () => 'font-bold text-base-content'
+    },
+  ];
+
+  public tableActions: TableAction<InvoiceResponseDto>[] = [
+    {
+      icon: 'pi-file-pdf',
+      label: 'Visualizar PDF',
+      colorClass: 'text-error hover:bg-error/10',
+      callback: (invoice) => this.viewInvoicePdf(invoice)
+    }
+  ];
+
+  constructor() {
+    effect(() => {
+      this.loadInvoices();
+    }, { allowSignalWrites: true });
+  }
+
+  ngOnInit(): void { }
+
+  /**
+   * Carga el listado de facturas
+   */
+  public loadInvoices(): void {
+    this.isLoading.set(true);
+
+    this.salesService.apiSalesGet(
+      this.page(),
+      this.pageSize(),
+      this.search()
+    ).subscribe({
+      next: (response: any) => {
+        if (response && response.success && response.data) {
+          this.invoices.set(response.data.items || []);
+          this.totalItems.set(response.data.totalItems || 0);
+          this.totalPages.set(response.data.totalPages || 0);
+        }
+      },
+      error: () => this.isLoading.set(false),
+      complete: () => this.isLoading.set(false)
+    });
+  }
+
+  /**
+   * Manejador del filtro de búsqueda con debounce
+   */
+  public handleSearch(term: string): void {
+    this.search.set(term);
+    this.page.set(1);
+  }
+
+  /**
+   * Abre el modal e inyecta el ID para que el visor descargue el flujo binario
+   */
+  public viewInvoicePdf(invoice: InvoiceResponseDto): void {
+    if (!invoice.id) return;
+    this.selectedInvoiceId.set(invoice.id);
+    this.pdfModal.open();
+  }
+
+  /**
+   * Limpia el estado al cerrar el modal para desmontar el iframe de memoria
+   */
+  public closePdfModal(): void {
+    this.pdfModal.close();
+    this.selectedInvoiceId.set(null);
+  }
+}
