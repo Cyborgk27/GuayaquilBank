c7e4743cbab90110abab2069fe964270bc2e873c
kcepeda <slenderstalin@hotmail.com>
2026-06-05 22:40:26 -0500

    feat(shared): se crea el layout

---
+++ GuayaquilBank.Frontend/src/app/shared/components/layout/layout/layout.ts
@@ -0,0 +1,33 @@
+import { Component, inject } from '@angular/core';
+import { AuthFacade } from '../../../../core/service/auth.facade';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-layout',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,       
+    RouterLinkActive
+  ],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css',
+})
+export class Layout {
+  public authService = inject(AuthFacade);
+  private router = inject(Router);
+
+  public menuItems = [
+    { label: 'Panel de Control', icon: 'pi-home', route: '/dashboard' },
+    { label: 'Clientes', icon: 'pi-users', route: '/customers' },
+    { label: 'Productos', icon: 'pi-box', route: '/products' },
+    { label: 'Facturación / Ventas', icon: 'pi-wallet', route: '/sales' },
+    { label: 'Configuración', icon: 'pi-cog', route: '/profile' },
+  ];
+
+  public onLogout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
