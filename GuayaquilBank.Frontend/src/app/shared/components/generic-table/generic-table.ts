d8ff4dc3d0d75e51c5d49f87024b83e5507943b6
kcepeda <slenderstalin@hotmail.com>
2026-06-05 20:38:46 -0500

    fix(shared): se agrega paginación a la tabla

--- GuayaquilBank.Frontend/src/app/shared/components/generic-table/generic-table.ts
+++ GuayaquilBank.Frontend/src/app/shared/components/generic-table/generic-table.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TableColumn } from '../../interfaces/table-column.interface';
 import { TableAction } from '../../interfaces/table-action.interface';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-generic-table',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './generic-table.html',
   styleUrl: './generic-table.css',
@@ -16,11 +17,25 @@ export class GenericTable<T extends Record<string, any>> {
   @Input() actions: TableAction<T>[] = [];
   @Input() emptyMessage: string = 'No se encontraron registros en este dominio.';
   @Input() emptyIcon: string = 'pi-box';
+  @Input() currentPage: number = 1;
+  @Input() totalPages: number = 0;
+  @Input() totalItems: number = 0;
+
+  public pageChange = output<number>();
 
   /**
    * Helper para obtener de forma segura el valor de una celda tipada
    */
   public getCellValue(row: T, key: string): any {
     return row[key];
+  }
+
+  /**
+   * Gestiona el cambio de página interno y emite el evento
+   */
+  public onPageChange(newPage: number): void {
+    if (newPage >= 1 && newPage <= this.totalPages && newPage !== this.currentPage) {
+      this.pageChange.emit(newPage);
+    }
   }
 }
